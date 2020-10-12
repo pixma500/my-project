5a799d6907c3bf260089793a3fc0ae2dff0565a0
SSS <svidinoff@mail.ru>
2020-10-12 10:00:35 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { Button, Spinner } from 'react-bootstrap'
 
 function App() {
+  const [value, setValue] = useState("")
+  const [results, setResults] = useState([])
+  const [loaded, setLoaded] = useState(false)
+  const [visible, setVisible] = useState(3)
+
+  useEffect(() => {
+    fetchImages()
+}, []
+)
+
+
+  const fetchImages = () => {
+    fetch(`http://api.unsplash.com/search/photos?client_id=GSMRDQzMVfpyzn8I36-SulYxX3xyPEVL84R_S7cGLfU&query=${value}`)
+      .then(res => res.json())
+      .then(data => {
+        let dat=data.results
+        dat.forEach((item,index)=>{
+          item['id']=index
+      } )
+        console.log(dat)
+        setResults(dat)
+      })
+      .then(() => setLoaded(true))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div className="mydiv">
+          <h4>Поиск</h4>
+          <input
+            style={{ width: '60%' }}
+            type="text" value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <Button onClick={() => fetchImages()} variant="success">Найти</Button>
+        </div>
+        <div  className="gallery">
+        {console.log(loaded)}
+          {!loaded ? <Spinner animation="border" /> 
+         
+            : results.map(item => {
+              return (
+                <div key={item.id} >
+                < img  src={item.urls.regular} alt={item.alt_description} />
+                  { item.alt_description }
+                  </div>
+                  )
+          }
+
+            )
+          }
+        </div>
+      </div>
+    </>
   );
 }
 
